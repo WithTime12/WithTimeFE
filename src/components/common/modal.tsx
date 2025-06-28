86cbabfe3c9539d6013696152f1a20f09b817525
김연진 <kyj0719@gmail.com>
2025-06-28 19:46:00 +0900

    style: 스타일링 추가

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -18,7 +18,7 @@ export default function Modal({ isOpen = true, children, onClose }: TModalprops)
     return createPortal(
         isVisible && (
             <div className="z-[1000] fixed top-0 left-0 w-[100vw] h-[100vh] bg-black/30 flex items-center justify-center">
-                <div className="relative bg-white p-[20px] flex flex-col">
+                <div className="relative bg-white p-[20px] flex flex-col rounding-16 shadow-default">
                     <div className="w-full flex justify-end p-[5px]" onClick={onClose}>
                         X
                     </div>
