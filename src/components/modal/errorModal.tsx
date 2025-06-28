4514ee30decd0446883c5d028accff63113d63e8
김연진 <kyj0719@gmail.com>
2025-06-28 19:41:00 +0900

    style: 스타일링 수정

--- src/components/modal/errorModal.tsx
+++ src/components/modal/errorModal.tsx
@@ -9,7 +9,7 @@ function ErrorModal({ onClose }: TErrorModalProps) {
     return (
         <Modal onClose={onClose}>
             {/* 모달의 내용은 children으로 전달해줍니다. 항상 Modal 컴포넌트 하위에 만들고 싶은 모달은 만들면됩니다. */}
-            <div className="flex flex-col items-center justify-center w-[300px] h-[150px] p-4 rounded">
+            <div className="flex flex-col items-center justify-center w-[300px] h-[150px] rounded p-4">
                 <div>에러</div>
             </div>
         </Modal>
