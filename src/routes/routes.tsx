9b42bee396d75fe6d7ace0366e3325ed350ea888
SEBINorSEBOUT <sebin030710@gmail.com>
2025-07-05 23:26:04 +0900

    feat: routes.tsx 생성

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,7 +3,16 @@ import { createBrowserRouter } from 'react-router-dom';
 import ModalProvider from '@/components/common/modalProvider';
 
 import Layout from '@/layout/layout';
+import FindPw from '@/pages/F.O-1/FindPw';
+import Join from '@/pages/F.O-1/JoinPage';
+import Login from '@/pages/F.O-1/LoginPage';
+import User from '@/pages/F.O-1/userSetting';
+import DateTest from '@/pages/F.O-2/dateTest';
 import Home from '@/pages/F.O-2/homePage';
+import Result from '@/pages/F.O-2/ResultPage';
+import Pay from '@/pages/F.O-3/paymentPage';
+import Withdraw from '@/pages/F.O-3/withdrawPage';
+import Course from '@/pages/F.O-4/coursePage';
 
 const router = createBrowserRouter([
     {
@@ -19,6 +28,42 @@ const router = createBrowserRouter([
             {
                 index: true,
                 element: <Home />,
+            },
+            {
+                path: 'find-pw',
+                element: <FindPw />,
+            },
+            {
+                path: 'join',
+                element: <Join />,
+            },
+            {
+                path: 'login',
+                element: <Login />,
+            },
+            {
+                path: 'usersetting',
+                element: <User />,
+            },
+            {
+                path: 'dateTest',
+                element: <DateTest />,
+            },
+            {
+                path: 'testResult',
+                element: <Result />,
+            },
+            {
+                path: 'payment',
+                element: <Pay />,
+            },
+            {
+                path: 'withdraw',
+                element: <Withdraw />,
+            },
+            {
+                path: 'course',
+                element: <Course />,
             },
         ],
     },
