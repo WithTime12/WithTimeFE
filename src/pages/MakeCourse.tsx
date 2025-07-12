eb11d81b37d755f913edfeec2a0b13e6a498dad8
김연진 <kyj0719@gmail.com>
2025-07-13 01:17:38 +0900

    feat: 중복 이미지 삭제

--- src/pages/MakeCourse.tsx
+++ src/pages/MakeCourse.tsx
@@ -1,3 +1,14 @@
+// import GraySvgButton from '@/components/common/';
+
+import DateCourseImg from '@/images/dateCourseTest.png';
+
 export default function MakeCourse() {
-    return <div />;
+    return (
+        <div className="flex w-full h-full">
+            <div className="flex ">
+                {/* <GraySvgButton /> */}
+                <img src={DateCourseImg} alt="" />
+            </div>
+        </div>
+    );
 }
