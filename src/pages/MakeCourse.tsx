b2d660ddef70932210f8168fe6785e1b8693a2e0
김연진 <kyj0719@gmail.com>
2025-07-13 01:27:29 +0900

    feat: 데이트 코스 만들기 랜딩페이지 제작 완료

--- src/pages/MakeCourse.tsx
+++ src/pages/MakeCourse.tsx
@@ -1,13 +1,28 @@
 // import GraySvgButton from '@/components/common/';
 
+import { useNavigate } from 'react-router-dom';
+
 import DateCourseImg from '@/images/dateCourseTest.png';
 
 export default function MakeCourse() {
+    const navigate = useNavigate();
     return (
-        <div className="flex w-full h-full">
-            <div className="flex ">
-                {/* <GraySvgButton /> */}
+        <div className="flex w-full h-full justify-center items-center py-[16px]">
+            <div className="flex w-[967px] max-w-[80vw] flex-col gap-[40px]">
+                {/* <GraySvgButton /> 추가 예정*/}
                 <img src={DateCourseImg} alt="" />
+                <div className="flex w-full text-center font-heading3 items-center justify-center">
+                    WithTime은 당신의 데이트 스타일과 상황을 분석해,
+                    <br /> 시간, 장소, 기분에 딱 맞는 맞춤형 코스를 자동으로 만들어줍니다.
+                </div>
+                <div className="flex w-full text-center items-center justify-center font-body1">
+                    몇 가지 정보만 입력하면,
+                    <br /> 당신을 위한 완벽한 하루가 시작됩니다.
+                    <br />
+                    계획은 필요 없어요 – 지금부터 데이트에만 집중하세요.
+                </div>
+                {/* 추후 공용컴포넌트 추가 예정 */}
+                <button onClick={() => navigate('/makeCourse/1')}>데이트 코스 만들러가기</button>
             </div>
         </div>
     );
