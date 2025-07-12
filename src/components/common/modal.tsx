ba4d49b7a5cde57c11767abf368f99ad768fa8e4
김연진 <kyj0719@gmail.com>
2025-07-13 01:42:21 +0900

    feat: 충돌 방지

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -17,14 +17,12 @@ export default function Modal({ isOpen = true, children, onClose }: TModalprops)
 
     return createPortal(
         isVisible && (
-            <div className="z-[1000] fixed inset-0 bg-black/30 overflow-y-auto">
-                <div className="w-full flex items-center justify-center">
-                    <div className="relative bg-white p-[20px] flex flex-col rounding-16 shadow-default">
-                        <div className="w-full flex justify-end p-[5px]" onClick={onClose}>
-                            X
-                        </div>
-                        <div className="flex">{children}</div>
+            <div className="z-[1000] fixed top-0 left-0 w-[100vw] h-[100vh] bg-black/30 flex items-center justify-center">
+                <div className="relative bg-white p-[20px] flex flex-col rounding-16 shadow-default">
+                    <div className="w-full flex justify-end p-[5px]" onClick={onClose}>
+                        X
                     </div>
+                    <div className="flex">{children}</div>
                 </div>
             </div>
         ),
