2d5a6dae1fdf133ee81c5a5f631d42f8d87fc1c2
김연진 <kyj0719@gmail.com>
2025-07-22 00:35:57 +0900

    fix: 문구 수정

--- src/pages/BookmarkedDateCourse.tsx
+++ src/pages/BookmarkedDateCourse.tsx
@@ -15,7 +15,7 @@ function BookmarkedDateCourse() {
             <div className="flex w-[1000px] max-w-[80vw] flex-col py-[24px] gap-[64px]">
                 <div className="flex flex-col shadow-default rounding-16 px-[40px] py-[24px]">
                     <div className="flex w-full gap-[16px] flex-col items-center py-[24px] sm:flex-row sm:justify-between sm:items-center">
-                        <div className="font-heading3 select-none flex">직접 데이트 코스 찾아보기</div>
+                        <div className="font-heading3 select-none flex">저장된 데이트 코스</div>
                         <div
                             onClick={() => openModal({ modalType: MODAL_TYPES.DateCourseSearchFilterModal })}
                             className="w-fit ml-auto px-[16px] flex-nowrap select-none py-[8px] rounding-16 flex rounding-16 border-[1px] border-default-gray-700 text-default-gray-700"
