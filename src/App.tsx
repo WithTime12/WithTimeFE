94d159e76910e8dfb48cf12252970dfe412c3c0b
SEBINorSEBOUT <sebin030710@gmail.com>
2025-07-21 17:23:32 +0900

    fix: 틀 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
+
+import Modal from './components/common/modal';
 
 import router from '@/routes/routes';
 
 function App() {
-    return <RouterProvider router={router} />;
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <RouterProvider router={router} />
+            <div>
+                <button onClick={() => setOpen(true)}>모달 열기</button>
+                <Modal isOpen={open} title="제목" onClose={() => setOpen(false)}>
+                    <div className="text-[22px]">내용</div>
+                </Modal>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/DateTestStep.tsx
@@ -0,0 +1,3 @@
+export default function DateTestStep() {
+    return <div />;
+}
