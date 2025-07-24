f47bc4862e45a75ab91e066cd370a7d45c397593
김연진 <kyj0719@gmail.com>
2025-07-25 00:37:46 +0900

    feat: 모바일 환경 margin 수정

--- src/layout/authLayout.tsx
+++ src/layout/authLayout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout() {
             <section className="hidden lg:block min-w-[200px] h-full bg-[linear-gradient(to_bottom,_white_0%,_white_40%,_#86e3c3_40%)]">
                 <img src={MainImg} alt="위티 메인 일러스트" className="h-full w-full object-contain" />
             </section>
-            <section className="flex-1 flex flex-col justify-center items-center px-[56px] h-full overflow-y-auto min-w-fit">
+            <section className="flex-1 flex flex-col justify-center items-center mb:px-[56px] h-full overflow-y-auto min-w-fit">
                 <Outlet />
             </section>
         </div>
