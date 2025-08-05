d63ffd190d2cbe3e1c2b3896b657279f59e00cc5
김연진 <kyj0719@gmail.com>
2025-08-05 17:30:45 +0900

    fix: navigate 삭제

--- src/components/alarmModal/alarm.tsx
+++ src/components/alarmModal/alarm.tsx
@@ -9,7 +9,7 @@ function Alarm({ title }: TAlarm) {
     return (
         <div className="flex itmes-center justify-between w-full py-[24px] border-b-[2px] border-b-default-gray-400">
             <div className="text-default-gray-800 text-[22px] sm:w-[500px] w-[200px] whitespace-nowrap overflow-hidden text-ellipsis">{title}</div>
-            <ChevronForward className="self-center" onClick={() => navigate('')} />
+            <ChevronForward className="self-center" />
         </div>
     );
 }
