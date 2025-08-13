10326fb5083cc77691b6e844d52c1ff7798f46f0
김연진 <kyj0719@gmail.com>
2025-08-13 15:56:55 +0900

    feat: 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,29 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
-    plugins: [react(), svgr({ include: '**/*.svg?react' }), tailwindcss()],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-    },
-    resolve: {
-        alias: {
-            '@': '/src',
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const target = env.VITE_API_BASE_URL;
+    return {
+        plugins: [react(), svgr({ include: '**/*.svg?react' }), tailwindcss()],
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            proxy: {
+                '/api': {
+                    target,
+                    changeOrigin: true,
+                    secure: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            },
         },
-    },
+        resolve: {
+            alias: {
+                '@': '/src',
+            },
+        },
+    };
 });
