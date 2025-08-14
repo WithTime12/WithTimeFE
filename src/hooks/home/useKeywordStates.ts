848a1e15c533c55bbc237f2196ee859e365c2d22
김연진 <kyj0719@gmail.com>
2025-08-14 18:49:37 +0900

    fix: 빌드 에러 해결

--- src/hooks/home/useKeywordStates.ts
+++ src/hooks/home/useKeywordStates.ts
@@ -5,7 +5,7 @@ import { homeKeys } from '@/queryKey/queryKey';
 
 // 이번 주 인기 키워드 훅
 export const useWeeklyKeywords = () => {
-    return useCoreQuery(homeKeys.keywords().queryKey, getWeeklyKeywords, {
+    return useCoreQuery(homeKeys.keywords.queryKey, getWeeklyKeywords, {
         gcTime: 30 * 60 * 1000,
         retry: 3,
     });
