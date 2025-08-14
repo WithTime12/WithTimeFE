5c2736bbbfda361e1191911f9ebbd93c9237080e
yujin5959 <jinjin17104@naver.com>
2025-08-14 15:28:12 +0900

    feat: 에러 컴포넌트 ui 완성

--- src/pages/common/Error.tsx
+++ src/pages/common/Error.tsx
@@ -1,3 +1,20 @@
+import Button from '@/components/common/Button';
+
+import AlertCircle from '@/assets/icons/alert-circle_Fill.svg?react';
+
 export default function Error() {
-    return <div />;
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center bg-white py-[100px]">
+                <AlertCircle className="w-[130px] h-[130px] mb-[80px] text-default-gray-500" />
+                <p className="text-3xl mb-[32px] text-default-gray-700 font-bold">서비스 이용에 불편을 드려 죄송합니다</p>
+                <p className="text-l text-default-gray-500 text-center mb-[160px]">
+                    요청하신 페이지를 찾을 수 없습니다 <br /> 경로가 잘못되었거나, 인터넷 연결이 불안정할 수 있습니다
+                </p>
+                <Button onClick={() => window.location.reload()} size={'big-16'} variant={'mint'} className="text-[22px]">
+                    이전 페이지로 돌아가기
+                </Button>
+            </div>
+        </>
+    );
 }
